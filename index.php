ba29e6941cb9ce0b17d528da257d7cc0d2fe7828
Ruben Yakunchev <86118746+gluck-59@users.noreply.github.com>
2022-11-11 19:35:05 +0500

    на тест

--- index.php
+++ index.php
@@ -1,31 +1,84 @@
 <?php
-    require_once 'settings.php';
 
-echo YOUTUBE_APIKEY;
-$res = get();
-print_r($res);
+    $youtube = new Youtube();
+    $res = $youtube->getVideosbyChannelId('UCndOFkIsRNaO0JyzxFwyR1w');
+    foreach ($res->items as $video) {
+        preg_match('/[\d]{5,7}/', $video->snippet->description, $matches);
+        $out[$video->id->videoId] = $matches[0];
+    }
 
-    function get()
-    {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, YOUTUBE_URL);
+//    echo '<pre>';
+//    print_r($out);
+?>
 
-//        curl_setopt($curl, CURLOPT_HEADER, true);
-//        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization:Bearer '.YOUTUBE_APIKEY, 'Accept:application/json']);
-//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        if (($response = curl_exec($curl)) === false) {
-            throw new \Exception(curl_error($curl));
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+        <title>Видео</title>
+        <link rel="stylesheet" href="//www.w3schools.com/w3css/4/w3.css">
+    </head>
+    <body>
+    <div class="w3-row">
+        <div class="w3-half w3-container">
+            <table class="w3-table-all">
+                <thead>
+                    <th>Ссылка</th>
+                    <th>Арт. №</th>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($out as $url => $item) { ?>
+                            <tr>
+                                <td><a href="https://www.youtube.com/watch?v=<?=$url?>" target="_blank">https://www.youtube.com/watch?v=<?=$url?></a></td>
+                                <td><?=$item?></td>
+                            </tr>
+                        <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+    </body>
+</html>
+
+
+<?php
+    class Youtube {
+        public $videos;
+
+        function __construct() {
+            require_once 'settings.php';
         }
 
+        /**
+         * тянет ID и описания роликов из заданного youtube-канала
+         * возвращает JSON
+         *
+         * @param string $channelId
+         * @return string
+         * @throws Exception
+         */
+        public function getVideosbyChannelId(string $channelId) {
+            $curl = curl_init();
+            $url = YOUTUBE_URL.'&channelId='.$channelId;
 
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ['Accept:application/json']);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
+            if (($response = curl_exec($curl)) === false) {
+                throw new \Exception(curl_error($curl));
+            }
+            curl_close($curl);
 
-        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-        curl_close($curl);
-
-//echo $header_size;
-        return $response;
-
-//    echo trim(substr($response, $header_size));
+            $this->videos = json_decode($response);
+            return $this->videos;
+        }
     }
+
+
